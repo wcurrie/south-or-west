1fbf734b3fc2797af6375fa2ef50fcac0098b8b9
Will Currie <will@currie.id.au>
2014-05-04 11:39:20 +1000

    Serve index.html. Surely a better way exists.

--- proxy.dart
+++ proxy.dart
@@ -10,20 +10,30 @@ void main() {
 
   HttpServer.bind(HOST, PORT).then((server) {
     server.listen((HttpRequest request) {
-      request.response
-        ..headers.set("Access-Control-Allow-Origin", "*")
-        ..headers.set("Access-Control-Allow-Credentials", "true")
-        ..headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE")
-        ..headers.set("Access-Control-Allow-Headers", "Content-Type, Accept, X-Request-With")
-        ..headers.set("Access-Control-Max-Age", "86400");
-
       print(request.uri);
 
-      http.get("http://www.bom.gov.au" + request.uri.toString()).then((http.Response response) {
-        request.response.headers.add("Content-Type", "application/json");
-        request.response.add(response.bodyBytes);
-        request.response.close();
-      });
+      if (request.uri.path == "/index.html") {
+        new File("index.html").readAsString().then((String s) {
+          request.response
+            ..headers.add("Content-Type", "text/html")
+            ..write(s)
+            ..close();
+        });
+      } else {
+        request.response
+          ..headers.set("Access-Control-Allow-Origin", "*")
+          ..headers.set("Access-Control-Allow-Credentials", "true")
+          ..headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE")
+          ..headers.set("Access-Control-Allow-Headers", "Content-Type, Accept, X-Request-With")
+          ..headers.set("Access-Control-Max-Age", "86400");
+
+        http.get("http://www.bom.gov.au" + request.uri.toString()).then((http.Response response) {
+          request.response
+            ..headers.add("Content-Type", "application/json")
+            ..add(response.bodyBytes)
+            ..close();
+        });
+      }
     });
   });
 
