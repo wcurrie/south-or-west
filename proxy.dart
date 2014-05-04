bbf07952e1f39d48980afc631ce587e0ceaf3e54
Will Currie <will@currie.id.au>
2014-05-04 13:06:06 +1000

    BOM data has most recent observation first.

--- proxy.dart
+++ proxy.dart
@@ -5,6 +5,7 @@ import 'package:http/http.dart' as http;
 
 final HOST = "0.0.0.0";
 final PORT = int.parse(Platform.environment.containsKey('PORT') ? Platform.environment['PORT'] : "5555");
+final LOCAL_MODE = Platform.executableArguments.contains('--local-mode');
 
 serveFile(String name, HttpRequest request, String type) {
   new File(name).readAsBytes().then((List<int> s) {
@@ -15,7 +16,24 @@ serveFile(String name, HttpRequest request, String type) {
   });
 }
 
+makeProxyRequest(HttpRequest request) {
+  request.response
+    ..headers.set("Access-Control-Allow-Origin", "*")
+    ..headers.set("Access-Control-Allow-Credentials", "true")
+    ..headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE")
+    ..headers.set("Access-Control-Allow-Headers", "Content-Type, Accept, X-Request-With")
+    ..headers.set("Access-Control-Max-Age", "86400");
+
+  http.get("http://www.bom.gov.au" + request.uri.toString()).then((http.Response response) {
+    request.response
+      ..headers.add("Content-Type", "application/json")
+      ..add(response.bodyBytes)
+      ..close();
+  });
+}
+
 void main() {
+  print(Platform.executableArguments);
 
   HttpServer.bind(HOST, PORT).then((server) {
     server.listen((HttpRequest request) {
@@ -27,19 +45,11 @@ void main() {
       } else if (file == "/favicon.ico") {
         serveFile("favicon.ico", request, "image/vnd.microsoft.icon");
       } else if (file.startsWith("/fwo")) {
-        request.response
-          ..headers.set("Access-Control-Allow-Origin", "*")
-          ..headers.set("Access-Control-Allow-Credentials", "true")
-          ..headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE")
-          ..headers.set("Access-Control-Allow-Headers", "Content-Type, Accept, X-Request-With")
-          ..headers.set("Access-Control-Max-Age", "86400");
-
-        http.get("http://www.bom.gov.au" + request.uri.toString()).then((http.Response response) {
-          request.response
-            ..headers.add("Content-Type", "application/json")
-            ..add(response.bodyBytes)
-            ..close();
-        });
+        if (LOCAL_MODE) {
+          serveFile(request.uri.pathSegments.last, request, "application/json");
+        } else {
+          makeProxyRequest(request);
+        }
       }
     });
   });
