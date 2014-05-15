8efa73442e2540be087236281ce642cc0e5a4445
Will Currie <will@currie.id.au>
2014-05-15 11:22:35 +1000

    Ditch bottom icons on mobile view.

--- public/mobile.js
+++ public/mobile.js
@@ -1,28 +1,15 @@
 angular.module('mobile', ['ionic', 'bom.plot', 'bom.observations']).config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider.state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  });
-  $stateProvider.state('tab.dash', {
+  $stateProvider.state('dash', {
     url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
+    templateUrl: 'templates/tab-dash.html',
+    controller: 'DashCtrl'
   });
-  $stateProvider.state('tab.stations', {
+  $stateProvider.state('stations', {
     url: '/stations',
-    views: {
-      'tab-stations': {
-        templateUrl: 'templates/tab-stations.html',
-        controller: 'StationsCtrl'
-      }
-    }
+    templateUrl: 'templates/tab-stations.html',
+    controller: 'StationsCtrl'
   });
-  return $urlRouterProvider.otherwise('/tab/dash');
+  return $urlRouterProvider.otherwise('/dash');
 }).controller('DashCtrl', function($scope, Preferences) {
   return $scope.stations = Preferences.load();
 }).controller('StationsCtrl', function($scope, Preferences) {
--- src/mobile.coffee
+++ src/mobile.coffee
@@ -1,32 +1,18 @@
 angular.module('mobile', ['ionic', 'bom.plot', 'bom.observations'])
 .config(($stateProvider, $urlRouterProvider) ->
   $stateProvider
-    .state('tab', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "templates/tabs.html"
-    })
-  $stateProvider
-    .state('tab.dash', {
+    .state('dash', {
       url: '/dash',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/tab-dash.html',
-          controller: 'DashCtrl'
-        }
-      }
+      templateUrl: 'templates/tab-dash.html',
+      controller: 'DashCtrl'
     })
   $stateProvider
-    .state('tab.stations', {
+    .state('stations', {
       url: '/stations',
-      views: {
-        'tab-stations': {
-          templateUrl: 'templates/tab-stations.html',
-          controller: 'StationsCtrl'
-        }
-      }
+      templateUrl: 'templates/tab-stations.html',
+      controller: 'StationsCtrl'
     })
-  $urlRouterProvider.otherwise('/tab/dash')
+  $urlRouterProvider.otherwise('/dash')
 )
 .controller('DashCtrl', ($scope, Preferences) ->
   $scope.stations = Preferences.load()
